b24fb6892cac0b91f5aceaafed57a5a0a5f991c5
Halis Uyanık <halisuyanik@outlook.com>
2021-10-11 03:24:10 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -6,13 +6,7 @@ import "firebase/database";
 
 
 const config={
-    apiKey: "AIzaSyAUuiWPgiGo3YxuKChJhkXuVq-gO0jf3Cg",
-    authDomain: "realtime-chat-vue-f0a48.firebaseapp.com",
-    databaseURL: "https://realtime-chat-vue-f0a48-default-rtdb.firebaseio.com",
-    projectId: "realtime-chat-vue-f0a48",
-    storageBucket: "realtime-chat-vue-f0a48.appspot.com",
-    messagingSenderId: "472907927659",
-    appId: "1:472907927659:web:319321b8cb7a6befcba12e"
+   *firebase db api keys *
 }
 
 const db = firebase.initializeApp(config);
